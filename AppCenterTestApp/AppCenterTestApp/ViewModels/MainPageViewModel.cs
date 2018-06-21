ccd55ac0a16efa2a82b7b9796c2370b837f3dcc8
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-21 19:47:24 +0200

    Add viewModel

---
+++ AppCenterTestApp/AppCenterTestApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppCenterTestApp.ViewModels
+{
+    public class MainPageViewModel : SimpleObservableObject
+    {
+        public String Title => "Bienvenue sur Xamarin.Forms !";
+
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { Set(ref number, value); }
+        }
+
+
+        public ICommand PlusCommand { private set; get; }
+
+        public MainPageViewModel()
+        {
+            PlusCommand = new Command(
+                execute: () =>
+                {
+                    Number++;
+                    RefreshCanExecutes();
+                },
+                canExecute: () =>
+        {
+            return Number < 10;
+        });
+        }
+
+
+
+        void RefreshCanExecutes()
+        {
+            (PlusCommand as Command).ChangeCanExecute();
+
+        }
+    }
+}
