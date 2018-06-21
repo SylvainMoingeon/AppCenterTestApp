f606264d8d4a4b4326117d98b3e4039b895fdfac
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-21 15:17:31 +0200

    Add AppCenter keys to app

--- AppCenterTestApp/AppCenterTestApp/App.xaml.cs
+++ AppCenterTestApp/AppCenterTestApp/App.xaml.cs
@@ -1,32 +1,37 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
-[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace AppCenterTestApp
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new MainPage();
-		}
+            MainPage = new MainPage();
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("android=7d096ecd-e96c-4236-bf94-a07de36fd26f;" +
+                            "uwp=28a878a9-5765-4242-8b85-d083e8d58299;", typeof(Analytics), typeof(Crashes));
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
