1528f319995e1ab64e85d41d4a75d95462f16114
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-22 20:51:38 +0200

    Add Test

--- AppCenterTestApp.UITest/Tests.cs
+++ AppCenterTestApp.UITest/Tests.cs
@@ -7,31 +7,42 @@ using Xamarin.UITest.Queries;
 
 namespace AppCenterTestApp.UITest
 {
-	[TestFixture(Platform.Android)]
-	[TestFixture(Platform.iOS)]
-	public class Tests
-	{
-		IApp app;
-		Platform platform;
+    //[TestFixture(Platform.iOS)]
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
 
-		public Tests(Platform platform)
-		{
-			this.platform = platform;
-		}
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
 
-		[SetUp]
-		public void BeforeEachTest()
-		{
-			app = AppInitializer.StartApp(platform);
-		}
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
 
-		[Test]
-		public void WelcomeTextIsDisplayed()
-		{
-			AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
-			app.Screenshot("Welcome screen.");
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "3")]
+        public void TapXTimesPlusButton_IncrementsXTimesIntegerEntry(int numberOfTaps, String entryTextResult)
+        {
+            // Arrange
 
-			Assert.IsTrue(results.Any());
-		}
-	}
+            // Act
+            for (int i = 0; i < numberOfTaps; i++)
+            {
+                app.Tap(x => x.Button("PlusButton"));
+                app.Screenshot("Tap Tap Number Button");
+            }
+
+
+            // Assert
+            var appResult = app.Query("IntegerEntry").First(result => result.Text.Equals(entryTextResult));
+        }
+    }
 }
