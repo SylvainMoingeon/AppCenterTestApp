0af8dc857db3bcb4998549eaacd92e23e3b517b5
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-23 12:21:23 +0200

    Remove --uitest-tools argument

--- AppCenterTestApp/AppCenterTestApp.Android/appcenter-post-build.sh
+++ AppCenterTestApp/AppCenterTestApp.Android/appcenter-post-build.sh
@@ -1,2 +1,2 @@
 msbuild $APPCENTER_SOURCE_DIRECTORY/AppCenterTestApp.UITest/AppCenterTestApp.UITest.csproj /p:Configuration=Release 
-appcenter test run uitest --app $APP --devices $DEVICES --app-path $APPCENTER_OUTPUT_DIRECTORY/fr.sylvainmoingeon.appcentertestapp.apk  --test-series "master" --locale "fr_FR" --build-dir $APPCENTER_SOURCE_DIRECTORY/AppCenterTestApp.UITest/bin/Release --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --async --token $TOKEN
+appcenter test run uitest --app $APP --devices $DEVICES --app-path $APPCENTER_OUTPUT_DIRECTORY/fr.sylvainmoingeon.appcentertestapp.apk  --test-series "master" --locale "fr_FR" --build-dir $APPCENTER_SOURCE_DIRECTORY/AppCenterTestApp.UITest/bin/Release --async --token $TOKEN
