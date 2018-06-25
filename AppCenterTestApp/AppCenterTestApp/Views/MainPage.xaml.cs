a5297cfb1f45b837b6279c599b9420648701d94f
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-25 18:24:24 +0200

    Add navigation to SecondPage from MainPage

--- AppCenterTestApp/AppCenterTestApp/Views/MainPage.xaml.cs
+++ AppCenterTestApp/AppCenterTestApp/Views/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace AppCenterTestApp.Views
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage(), true);
+        }
+    }
 }
