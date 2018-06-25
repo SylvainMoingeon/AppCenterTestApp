1dd50b317e740c108998bb96243b4329773230a6
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-25 18:23:58 +0200

    Add a second page

---
+++ AppCenterTestApp/AppCenterTestApp/ViewModels/SecondPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCenterTestApp.ViewModels
+{
+    public class SecondPageViewModel : SimpleObservableObject
+    {
+
+        public String Title => "Ma fantastique seconde page !";
+
+        public SecondPageViewModel()
+        {
+
+        }
+
+
+    }
+}
