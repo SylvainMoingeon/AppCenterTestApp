225dc7f507ff52f21aba2a3befaca10920835a4c
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-26 11:49:00 +0200

    Add WaitTime helper to avoid timeout false positive

---
+++ AppCenterTestApp.UITest/UITestHelpers/WaitTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest.Utils;
+
+namespace AppCenterTestApp.UITest.UITestHelpers
+{
+    public class WaitTimes : IWaitTimes
+    {
+        public TimeSpan GestureWaitTimeout
+        {
+            get { return TimeSpan.FromMinutes(1); }
+        }
+        public TimeSpan WaitForTimeout
+        {
+            get { return TimeSpan.FromMinutes(1); }
+        }
+
+        public TimeSpan GestureCompletionTimeout
+        {
+            get { return TimeSpan.FromSeconds(5); }
+        }
+    }
+}
