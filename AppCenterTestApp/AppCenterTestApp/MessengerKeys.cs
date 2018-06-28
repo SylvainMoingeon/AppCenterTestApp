7a501df69b20813fd28723a3c7b1f6b414bf2af6
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-06-28 18:36:27 +0200

    Add constant for messaging

---
+++ AppCenterTestApp/AppCenterTestApp/MessengerKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCenterTestApp
+{
+    public class MessengerKeys
+    {
+        public const String LoginSuccess = "LoginSuccess";
+        public const String LoginFailure = "LoginFailure";
+    }
+}
