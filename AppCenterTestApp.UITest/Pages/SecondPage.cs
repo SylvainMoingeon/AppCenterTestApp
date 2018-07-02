adcf7eb4549ddc96f1169d377afd5d6d46a953f1
Sylvain Moingeon <sylvain.moingeon@outlook.fr>
2018-07-02 18:00:31 +0200

    Add SecondPage

---
+++ AppCenterTestApp.UITest/Pages/SecondPage.cs
@@ -0,0 +1,21 @@
+﻿using AppCenterTestApp.UITest.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace AppCenterTestApp.UITest.Pages
+{
+
+    public class SecondPage : BasePage
+    {
+        const string SecondPageName = "Second Page";
+        const string SecondPageAutomationId = "SecondPage";
+
+        public SecondPage(IApp app) : base(app, SecondPageName, SecondPageAutomationId)
+        {
+        }
+    }
+}
